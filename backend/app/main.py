6405758c1ca89ddf2ccf9ff63d734cfcc0783550
ZakPorat <zakporat2023@gmail.com>
2024-07-03 23:16:36 +0100

    feat: create chat engine settings route

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,16 @@
 import logging
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
+from models import HTTPRequest, ChatEngineSettings
+from engine.settings import init_settings
+from engine.index.generate_index import generate_datasource
+
 app = FastAPI()
 
-logger = logging.getLogger('uvicorn')
+logger = logging.getLogger("uvicorn")
+
 
 environment = "dev"
 if environment == "dev":
@@ -22,5 +27,37 @@ app.get('/')
 async def index():
     return {"message": "hello world!"}
 
+app.get('/chat-engine-settings')
+async def engine_settings(data: ChatEngineSettings):
+    try:
+
+        model = data.model_name
+        llm_temp = data.llm_temperature
+        embedding_model = data.embedding_model
+
+        init_settings(model, llm_temp, embedding_model)
+
+        file = data.file
+        web_url = data.web_url
+
+        generate_datasource(file, web_url)
+
+        return HTTPRequest(
+            status_code=201,
+            message="Chat engine settings initialized successfully"
+        )
+
+    except HTTPException:
+        raise
+
+    except Exception as e:
+        logger.error(f"Error getting chat engine settings {str(e)}")
+
+        raise HTTPException(
+            status_code=500,
+            detail=f"Error getting chat engine settings {str(e)}"
+        )
+
+
 if __name__ == '__main__':
-    uvicorn.run('main:app', host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
