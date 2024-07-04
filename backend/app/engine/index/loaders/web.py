02488cf46e79842652f7a424d9f250ea86864643
ZakPorat <zakporat2023@gmail.com>
2024-07-04 18:50:35 +0100

    fix: crawl url class error

--- backend/app/engine/index/loaders/web.py
+++ backend/app/engine/index/loaders/web.py
@@ -1,32 +1,27 @@
+import logging
 from pydantic import BaseModel, Field
 from typing import List
 
+logger = logging.getLogger("uvicorn")
+
 class CrawlUrl(BaseModel):
     base_url: str
-    prefix: str
-    max_depth: int = Field(default=1, ge=0)
-
-class WebLoaderConfig(BaseModel):
-    driver_arguments: list[str] = Field(default=None)
-    urls: list[CrawlUrl]
+    prefix: str = ""
+    # max_depth: int = Field(default=1, ge=0)
+    max_depth: int = 0 # for testing purposes only
 
 def get_web_documents(urls: List[str]):
-    config = WebLoaderConfig(urls=[CrawlUrl(base_url=url) for url in urls])
+    config = [CrawlUrl(base_url=url) for url in urls]
 
     from llama_index.readers.web import WholeSiteReader
     from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
-
-    options = Options()
-    for arg in config.driver_arguments:
-        options.add_argument(arg)
 
     docs = []
-    for url in config.urls:
+    for url in config:
         scraper = WholeSiteReader(
             prefix=url.prefix,
             max_depth=url.max_depth,
-            driver=webdriver.Chrome(options=options)
+            driver=webdriver.Chrome()
         )
         docs.extend(scraper.load_data(url.base_url))
 
