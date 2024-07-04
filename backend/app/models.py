d5f299419241a48c4dae381f5605a68a1a70f284
ZakPorat <zakporat2023@gmail.com>
2024-07-04 18:51:35 +0100

    refractor: file and web_url optional

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,16 +1,17 @@
 from pydantic import BaseModel
 from fastapi import UploadFile
+from typing import Optional, List
 
 class HTTPRequest(BaseModel):
     status_code: int
     message: str
 
 class ChatEngineSettings(BaseModel):
-    model_name: str
+    model: str
     llm_temperature: float
     embedding_model: str
-    file: UploadFile
-    web_url: str
+    file: Optional[UploadFile] = None
+    web_url: Optional[List[str]] = None
 
     class Config:
         arbitrary_types_allowed = True
