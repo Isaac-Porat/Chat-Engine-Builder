e4730ddc3d5d6bef1c2842762b0b8a1d030ea4c4
ZakPorat <zakporat2023@gmail.com>
2024-07-04 18:51:15 +0100

    feat: add get-index route

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,13 +4,16 @@ from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from models import HTTPRequest, ChatEngineSettings
-from engine.settings import init_settings
+from engine.settings import set_settings
 from engine.index.generate_index import generate_datasource
+from engine.api.routers.chat import chat_router
+from engine.index.index import get_index
 
 app = FastAPI()
 
 logger = logging.getLogger("uvicorn")
 
+app.include_router(chat_router, prefix="/api/chat")
 
 environment = "dev"
 if environment == "dev":
@@ -23,24 +26,24 @@ if environment == "dev":
         allow_headers=["*"],
     )
 
-app.get('/')
+@app.get('/')
 async def index():
     return {"message": "hello world!"}
 
-app.get('/chat-engine-settings')
+@app.post('/chat-engine-settings')
 async def engine_settings(data: ChatEngineSettings):
     try:
 
-        model = data.model_name
+        model = data.model
         llm_temp = data.llm_temperature
         embedding_model = data.embedding_model
 
-        init_settings(model, llm_temp, embedding_model)
+        set_settings(model, llm_temp, embedding_model)
 
         file = data.file
-        web_url = data.web_url
+        web_urls = data.web_url
 
-        generate_datasource(file, web_url)
+        generate_datasource(file, web_urls)
 
         return HTTPRequest(
             status_code=201,
@@ -58,6 +61,20 @@ async def engine_settings(data: ChatEngineSettings):
             detail=f"Error getting chat engine settings {str(e)}"
         )
 
+@app.get('/get-index')
+async def get_current_index():
+    index = get_index()
+
+    if not index:
+        return HTTPException(
+            status_code=404,
+            detail="Index not found"
+        )
+
+    return HTTPRequest(
+        status_code=200,
+        message="Index found"
+    )
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
